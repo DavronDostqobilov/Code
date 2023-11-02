8c771ccdc610506465a07ffa8b71ff4175461bad
DavronDostqobilov <dostqobilovdavron885@gamil.com>
2023-11-02 13:16:56 +0500

    create the blog

---
+++ homepage/tests.py
@@ -0,0 +1,15 @@
+from django.test import SimpleTestCase
+
+# Create your tests here.
+
+class SimpleTests(SimpleTestCase):
+    def test_home(self):
+        response=self.client.get('/')
+        self.assertEqual(response.status_code,200)
+    def test_about(self):
+        response=self.client.get('/about/')
+        self.assertEqual(response.status_code,200)
+    def test_next(self):
+        response=self.client.get('/next/')
+        self.assertEqual(response.status_code,200)
+    
